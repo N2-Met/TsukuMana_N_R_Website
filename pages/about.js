27201fd035c5db9d05856a1850f4d6a0ae0250d8
N2-Met <yuyushiki11@gmail.com>
2023-01-03 15:03:17 +0900

    update

--- pages/about.js
+++ pages/about.js
@@ -2,6 +2,8 @@ import Container from "components/container";
 import Hero from "components/hero";
 import PostBody from "components/post-body";
 import Contact from "components/contact";
+import Image from "next/image";
+import eyecatch from "images/about.jpg";
 import {
   TwoColumn,
   TwoColumnMain,
@@ -12,6 +14,17 @@ export default function About() {
   return (
     <Container>
       <Hero title="About" subtitle="About development activities" />
+
+      <figure>
+        <Image
+          src={eyecatch}
+          alt=""
+          layout="responsive"
+          sizes="(min-width:1152px) 1152px,100vw"
+          privority
+          placeholder="blur"
+        />
+      </figure>
 
       <TwoColumn>
         <TwoColumnMain>
