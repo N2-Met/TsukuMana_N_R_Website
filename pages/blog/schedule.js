6c6222312b19919b2f604f4e5a6c044bb0f266d6
N2-Met <yuyushiki11@gmail.com>
2023-01-28 12:38:32 +0900

    update

--- pages/blog/schedule.js
+++ pages/blog/schedule.js
@@ -12,6 +12,10 @@ import {
 import ConvertBody from "components/convert-body";
 import PostCategories from "components/post-categories";
 import Image from "next/image";
+import { getPlaiceholder } from "plaiceholder";
+
+//ローカルの代替アイキャッチ画像
+import { eyecatchLocal } from "lib/constants";
 
 export default function Schedule({
   title,
@@ -41,6 +45,8 @@ export default function Schedule({
             height={eyecatch.height}
             sizes="(min-width: 1152px)1152px,110vw"
             priority
+            placeholder="blur"
+            blurDataURL={eyecatch.blurDataURL}
           />
         </figure>
         <TwoColumn>
@@ -60,17 +66,23 @@ export default function Schedule({
 }
 
 export async function getStaticProps() {
-  const slug = "schedule";
+  // const slug = "schedule";
+  const slug = "micro";
 
   const post = await getPostBySlug(slug);
   const description = extractText(post.content);
+  // "hoge??foo"左辺hogeがnullまたはundefinedの場合に右辺を返す。
+  const eyecatch = post.eyecatch ?? eyecatchLocal;
+
+  const { base64 } = await getPlaiceholder(eyecatch.url);
+  eyecatch.blurDataURL = base64;
 
   return {
     props: {
       title: post.title,
       publish: post.publishDate,
       content: post.content,
-      eyecatch: post.eyecatch,
+      eyecatch: eyecatch,
       categories: post.categories,
       description: description,
     },
