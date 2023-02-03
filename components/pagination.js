72d4c0524258e347ff1a29301a0c9f2f6e2c80ac
N2-Met <yuyushiki11@gmail.com>
2023-02-04 08:51:34 +0900

    update

---
+++ components/pagination.js
@@ -0,0 +1,16 @@
+import styles from 'styles/pagination.module.css'
+import link from 'next/link'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faChevronLeft, faChevronRight } from '@fortawesome/free-solid-svg-icons'
+
+export default function Pagination({
+    prevText = '',
+    prevUrl = '',
+    prevText = '',
+    nextText = '',
+    nextUrl = '',
+}) {
+    return (
+        
+    )
+}
