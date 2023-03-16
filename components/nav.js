cc5d75d1c8aab667645002b39b77ccdd5fa458b0
N2-Met <yuyushiki11@gmail.com>
2023-03-16 22:52:17 +0900

    update

--- components/nav.js
+++ components/nav.js
@@ -1,10 +1,20 @@
+import { useState } from "react";
 import Link from "next/link";
 import styles from "styles/nav.module.css";
 
 export default function Nav() {
+  const [navIsOpen, setNavIsOpen] = useState(false);
+
+  const toggleNav = () => {
+    setNavIsOpen((prev) => !prev);
+  };
+
   return (
-    <nav>
-      <button className={styles.btn}> MENU</button>
+    <nav className={navIsOpen ? styles.open : styles.close}>
+      <button className={styles.btn} onClick={toggleNav}>
+        {" "}
+        MENU
+      </button>
 
       <ul className={styles.list}>
         <li>
