a9a8eeef90bead11e1698dc2abb261690f629cec
N2-Met <yuyushiki11@gmail.com>
2023-05-06 11:11:33 +0900

    update

--- next.config.js
+++ next.config.js
@@ -3,6 +3,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
+    // P.334　静的サイトジェネレーターとして出力する　miss
     // loader: "imgix",
     // path: "https://images.microcms-assets.io/assets/",
     domains: ["images.microcms-assets.io"],
